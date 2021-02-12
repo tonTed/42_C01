8a57a75f9dd505e0ee8c0a19d45775e5c911e449
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-12 11:07:12 -0500

    ex 04 norminette OK

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,11 +6,9 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 10:55:46 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/12 11:01:57 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/12 11:06:49 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
@@ -19,16 +17,4 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	mod = *a % *b;
 	*a = *a / *b;
 	*b = mod;
-}
-
-int main()
-{
-	int a;
-	int b;
-
-	a = 42;
-	b = 10;
-	printf("a = %d && b = %d\n", a, b);
-	ft_ultimate_div_mod(&a, &b);
-	printf("a = %d && b = %d\n", a, b);
 }
