f56f06d1241a79122211b0dad4621e66e21f7709
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-12 08:42:15 -0500

    ex02 Norminette OK

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -6,11 +6,9 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 08:36:27 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/12 08:41:17 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/12 08:41:56 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
@@ -19,19 +17,4 @@ void	ft_swap(int *a, int *b)
 	swap = *a;
 	*a = *b;
 	*b = swap;
-}
-
-int main()
-{
-	int a;
-	int b;
-	
-	a = 42;
-	b = -42;
-
-	printf("a = %d & b = %d\n", a, b);
-	ft_swap(&a, &b);
-	printf("a = %d & b = %d\n", a, b);
-
-	return 0;
 }
