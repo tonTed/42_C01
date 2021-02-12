38baff3fdcfec7e8e09707e2fad50643806ccfd0
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-12 08:28:32 -0500

    ex00 norminette ok

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -3,30 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/11 17:48:31 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/11 18:05:54 by tonted           ###   ########.fr       */
+/*   Updated: 2021/02/12 08:28:19 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-void    ft_ft(int *nbr)
+void	ft_ft(int *nbr)
 {
-    printf("%d\n", *nbr);
-    *nbr = 42;
-    printf("%d\n", *nbr);
-
-}
-
-int main()
-{
-    int numb;
-    int *ptr;
-
-    ptr = &numb;
-    numb = 12;
-    ft_ft(ptr);
-    printf("%d\n", numb);
+	*nbr = 42;
 }
