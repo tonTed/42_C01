27a783905967af3a997306cade3bd31e0a9a1381
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-13 18:38:47 -0500

    ex08 norminette ok

--- ex08/test.c
+++ ex08/test.c
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 17:45:56 by tblanco           #+#    #+#             */
-/*   Updated: 2021/02/13 18:38:21 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/13 18:36:56 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = swap;
 }
+
+
+
 
 void	ft_sort_int_tab(int *tab, int size)
 {
@@ -40,4 +43,38 @@ void	ft_sort_int_tab(int *tab, int size)
 			}
 		}
 	}
+}
+
+int main()
+{
+    int tab[10];
+    int i;
+
+	tab[0] = 1532165456;
+	tab[1] = 15;
+	tab[2] = 95;
+	tab[3] = -1;
+	tab[4] = -5000;
+	tab[5] = 0;
+	tab[6] = 4168;
+	tab[7] = 856;
+	tab[8] = 56;
+	tab[9] = -55885454;
+
+    i = 1;
+    while(i < sizeof(tab)/4)
+    {
+        printf("%d\n", tab[i]);
+        i++;
+    }
+    printf("++++++++++++++++++\n");
+	ft_sort_int_tab(tab, sizeof(tab)/4);
+    
+    i = 0;
+    while(i < sizeof(tab)/4)
+    {
+        printf("%d\n", tab[i]);
+        i++;
+    }
+    return 0;
 }
